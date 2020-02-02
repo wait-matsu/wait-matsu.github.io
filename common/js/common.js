eda832f66bbac0a0be48e704d1d55b76cdb31b91
matsumura <ikurauryaaaaa@yahoo.co.jp>
2020-02-02 11:15:34 +0900

    SP時のアンカーメニュークリック時にメニューを閉じる処理追加

--- common/js/common.js
+++ common/js/common.js
@@ -6,7 +6,18 @@ const scroll = new SmoothScroll('a[href*="#"]');
 /* ハンバーガーメニュークラス切替
    ============================================ */
 
-const hamburgerMenu = document.querySelector('header .hamburger-menu');
-hamburgerMenu.addEventListener('click', () => {
-  hamburgerMenu.classList.toggle('is-active');
-})
+if (window.innerWidth < 1000) {
+
+  const hamburgerMenu = document.querySelector('header .hamburger-menu');
+  hamburgerMenu.addEventListener('click', () => {
+    hamburgerMenu.classList.toggle('is-active');
+  });
+
+  // メニュー内のリンククリック時にメニューを閉じる
+  const hamburgerMenuItems = document.querySelectorAll('header .hamburger-menu+.global-menu li:not(.sns)');
+  hamburgerMenuItems.forEach(li => {
+    li.addEventListener('click', () => {
+      hamburgerMenu.classList.remove('is-active');
+    });
+  })
+}
