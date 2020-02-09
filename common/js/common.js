18d9f5dca9b00054c3c6d1226aea17986ab9811d
matsumura <ikurauryaaaaa@yahoo.co.jp>
2020-02-09 17:47:18 +0900

    ページトップボタン追加

--- common/js/common.js
+++ common/js/common.js
@@ -3,21 +3,49 @@
 
 const scroll = new SmoothScroll('a[href*="#"]');
 
+
+/* ページトップボタン挿入
+   ============================================ */
+
+   const pageTopBtn = document.createElement('a');
+   pageTopBtn.setAttribute('href', '#');
+   pageTopBtn.classList.add('page-top');
+   pageTopBtn.classList.add('is-not-visible');
+
+   document.querySelector('main').appendChild(pageTopBtn);
+
+
+   const insertedPageTopBtn = document.querySelector('main a.page-top');
+   window.addEventListener('scroll', () => {
+  
+    //高さを取得して表示
+    let scrollTop = document.scrollingElement.scrollTop;
+  if(scrollTop > 100) {
+    insertedPageTopBtn.classList.remove('is-not-visible');
+  }else{
+    insertedPageTopBtn.classList.add('is-not-visible');
+  }
+  
+    
+  }, false);
+
 /* ハンバーガーメニュークラス切替
    ============================================ */
 
 if (window.innerWidth < 1000) {
 
   const hamburgerMenu = document.querySelector('header .hamburger-menu');
-  hamburgerMenu.addEventListener('click', () => {
-    hamburgerMenu.classList.toggle('is-active');
-  });
-
-  // メニュー内のリンククリック時にメニューを閉じる
-  const hamburgerMenuItems = document.querySelectorAll('header .hamburger-menu+.global-menu li:not(.sns)');
-  hamburgerMenuItems.forEach(li => {
-    li.addEventListener('click', () => {
-      hamburgerMenu.classList.remove('is-active');
+  if(hamburgerMenu){
+    hamburgerMenu.addEventListener('click', () => {
+      hamburgerMenu.classList.toggle('is-active');
     });
-  })
+  
+    // メニュー内のリンククリック時にメニューを閉じる
+    const hamburgerMenuItems = document.querySelectorAll('header .hamburger-menu+.global-menu li:not(.sns)');
+    hamburgerMenuItems.forEach(li => {
+      li.addEventListener('click', () => {
+        hamburgerMenu.classList.remove('is-active');
+      });
+    })
+  }
 }
